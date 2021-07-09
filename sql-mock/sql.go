66aa586096bdec59449065e5c4375d21abb7618b
Bahdan Matviyets <Bahdan.Matviyets@itechart-group.com>
2021-07-09 11:48:24 +0300

    scan NULL values for User

--- sql-mock/sql.go
+++ sql-mock/sql.go
@@ -3,22 +3,45 @@ package sqlmock
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
-	ID       int
-	Email    string
-	Password string
+	ID             int
+	Email          string
+	Password       string
+	ActivationDate time.Time
 }
 
-func GetUser(db *sql.DB, id int) (User, error) {
-	const query = "SELECT id, email, password FROM users WHERE id = ?"
-	var u User
-	err := db.QueryRow(query, id).Scan(&u.ID, &u.Email, &u.Password)
+type dbUser struct {
+	ID             int
+	Email          string
+	Password       string
+	ActivationDate sql.NullTime
+}
+
+func (dbu dbUser) toUser() User {
+	return User{
+		ID:             dbu.ID,
+		Email:          dbu.Email,
+		Password:       dbu.Password,
+		ActivationDate: dbu.ActivationDate.Time,
+	}
+}
+
+func GetUser(db *sql.DB, userID int) (User, error) {
+	const query = "SELECT id, email, password, activation_date FROM users WHERE id = ?"
+	var dbu dbUser
+	err := db.QueryRow(query, userID).Scan(
+		&dbu.ID,
+		&dbu.Email,
+		&dbu.Password,
+		&dbu.ActivationDate,
+	)
 	if err != nil {
 		return User{}, fmt.Errorf("cannot get user from DB %w", err)
 	}
-	return u, nil
+	return dbu.toUser(), nil
 }
--- sql-mock/sql_test.go
+++ sql-mock/sql_test.go
@@ -1,10 +1,19 @@
 package sqlmock
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 )
+
+// TODO:
+// implement toDBUser() + DTO* - prosharit'sa
+// COALESCE - to read to whom it may concern
+// args for prepare()
+// common tests for success and error cases
+// add error cases (no rows, db error)
 
 func TestGetUser(t *testing.T) {
 	db, mock, err := sqlmock.New()
@@ -12,23 +21,60 @@ func TestGetUser(t *testing.T) {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 
-	expectedUser := User{ID: 42, Email: "test@gmail.com", Password: "123456"}
-
-	mock.ExpectQuery("SELECT id, email, password FROM users WHERE id = (.*)").
-		WithArgs(42).
-		WillReturnRows(sqlmock.NewRows([]string{"id", "email", "password"}).
-			AddRow(42, "test@gmail.com", "123456"))
-
-	gotUser, err := GetUser(db, 42)
-	if err != nil {
-		t.Errorf("an error '%s' was not expected when getting user by ID", err)
+	cases := []struct {
+		name         string
+		expectedUser User
+		prepare      func(User)
+	}{
+		{
+			name: "success",
+			expectedUser: User{
+				ID:             42,
+				Email:          "test@gmail.com",
+				Password:       "123456",
+				ActivationDate: time.Time{},
+			},
+			prepare: func(u User) {
+				mock.ExpectQuery("SELECT (.*) FROM users WHERE id = (.*)").
+					WithArgs(u.ID).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "email", "password", "activation_date"}).
+						AddRow(u.ID, u.Email, u.Password, u.ActivationDate))
+			},
+		},
+		{
+			name: "NULL date",
+			expectedUser: User{
+				ID:             42,
+				Email:          "test@gmail.com",
+				Password:       "123456",
+				ActivationDate: time.Time{},
+			},
+			prepare: func(u User) {
+				mock.ExpectQuery("SELECT (.*) FROM users WHERE id = (.*)").
+					WithArgs(u.ID).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "email", "password", "activation_date"}).
+						AddRow(u.ID, u.Email, u.Password, nil))
+			},
+		},
 	}
 
-	if expectedUser != gotUser {
-		t.Errorf("expected user to be %+v, but got %+v", expectedUser, gotUser)
-	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.prepare(tc.expectedUser)
 
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
+			gotUser, err := GetUser(db, tc.expectedUser.ID)
+			fmt.Printf("%+v", gotUser)
+			if err != nil {
+				t.Errorf("an error '%s' was not expected when getting user by ID", err)
+			}
+
+			if tc.expectedUser != gotUser {
+				t.Errorf("expected user to be %+v, but got %+v", tc.expectedUser, gotUser)
+			}
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
 	}
 }
