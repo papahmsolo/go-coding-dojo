58181ceb7f26db4ede5bef74f9bf97953540c7af
Bahdan Matviyets <Bahdan.Matviyets@itechart-group.com>
2021-07-16 11:50:57 +0300

    add test for error cases

--- sql-mock/sql.go
+++ sql-mock/sql.go
@@ -2,11 +2,14 @@ package sqlmock
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var ErrNotFound = errors.New("no such user")
 
 type User struct {
 	ID             int
@@ -40,8 +43,36 @@ func GetUser(db *sql.DB, userID int) (User, error) {
 		&dbu.Password,
 		&dbu.ActivationDate,
 	)
+	if err == sql.ErrNoRows {
+		return User{}, fmt.Errorf("%w: %d", ErrNotFound, userID)
+	}
 	if err != nil {
 		return User{}, fmt.Errorf("cannot get user from DB %w", err)
 	}
 	return dbu.toUser(), nil
+}
+
+func UpdateUser(db *sql.DB, u User) error {
+	const query = "UPDATE users SET email=?, password=? WHERE id = ?"
+	res, err := db.Exec(query, u.Email, u.Password, u.ID)
+	if err != nil {
+		return fmt.Errorf("cannot update user in DB: %w", err)
+	}
+	n, err := res.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("cannot get RowsAffected result: %w", err)
+	}
+	if n == 0 {
+		return fmt.Errorf("user was not updated. %w: %d", ErrNotFound, u.ID)
+	}
+	return nil
+}
+
+func InsertUser(db *sql.DB, u User) error {
+	const query = "INSERT INTO users (email, password, activation_date) VALUES(?, ?, ?)"
+	_, err := db.Exec(query, u.Email, u.Password, u.ActivationDate)
+	if err != nil {
+		return fmt.Errorf("cannot insert user into DB: %w", err)
+	}
+	return nil
 }
--- sql-mock/sql_test.go
+++ sql-mock/sql_test.go
@@ -1,7 +1,8 @@
 package sqlmock
 
 import (
-	"fmt"
+	"database/sql"
+	"errors"
 	"testing"
 	"time"
 
@@ -12,8 +13,7 @@ import (
 // implement toDBUser() + DTO* - prosharit'sa
 // COALESCE - to read to whom it may concern
 // args for prepare()
-// common tests for success and error cases
-// add error cases (no rows, db error)
+// not catching sql syntax error - write concrete queries with regex
 
 func TestGetUser(t *testing.T) {
 	db, mock, err := sqlmock.New()
@@ -21,10 +21,13 @@ func TestGetUser(t *testing.T) {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 
+	var testErr = errors.New("test error")
+
 	cases := []struct {
-		name         string
-		expectedUser User
-		prepare      func(User)
+		name          string
+		expectedUser  User
+		expectedError error
+		prepare       func(User)
 	}{
 		{
 			name: "success",
@@ -56,6 +59,26 @@ func TestGetUser(t *testing.T) {
 						AddRow(u.ID, u.Email, u.Password, nil))
 			},
 		},
+		{
+			name:          "user not found",
+			expectedUser:  User{},
+			expectedError: ErrNotFound,
+			prepare: func(u User) {
+				mock.ExpectQuery("SELECT (.*) FROM users WHERE id = (.*)").
+					WithArgs(u.ID).
+					WillReturnError(sql.ErrNoRows)
+			},
+		},
+		{
+			name:          "db call error",
+			expectedUser:  User{},
+			expectedError: testErr,
+			prepare: func(u User) {
+				mock.ExpectQuery("SELECT (.*) FROM users WHERE id = (.*)").
+					WithArgs(u.ID).
+					WillReturnError(testErr)
+			},
+		},
 	}
 
 	for _, tc := range cases {
@@ -63,13 +86,80 @@ func TestGetUser(t *testing.T) {
 			tc.prepare(tc.expectedUser)
 
 			gotUser, err := GetUser(db, tc.expectedUser.ID)
-			fmt.Printf("%+v", gotUser)
-			if err != nil {
-				t.Errorf("an error '%s' was not expected when getting user by ID", err)
+			//fmt.Printf("%+v", gotUser)
+			if !errors.Is(err, tc.expectedError) {
+				t.Errorf("expected error to be %v, but got %v", tc.expectedError, err)
 			}
 
 			if tc.expectedUser != gotUser {
 				t.Errorf("expected user to be %+v, but got %+v", tc.expectedUser, gotUser)
+			}
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
+
+func TestInsertUser(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	var testErr = errors.New("test error")
+
+	cases := []struct {
+		name          string
+		args          User
+		expectedError error
+		prepare       func(User)
+	}{
+		{
+			name: "success",
+			prepare: func(u User) {
+				mock.ExpectExec("UPDATE users SET (.*) WHERE id = ?").
+					WithArgs(u.Email, u.Password, u.ID).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+			},
+		},
+		{
+			name:          "no such user",
+			expectedError: ErrNotFound,
+			prepare: func(u User) {
+				mock.ExpectExec("UPDATE users SET (.*) WHERE id = ?").
+					WithArgs(u.Email, u.Password, u.ID).
+					WillReturnResult(sqlmock.NewResult(0, 0))
+			},
+		},
+		{
+			name:          "db call error",
+			expectedError: testErr,
+			prepare: func(u User) {
+				mock.ExpectExec("UPDATE users SET (.*) WHERE id = ?").
+					WithArgs(u.Email, u.Password, u.ID).
+					WillReturnError(testErr)
+			},
+		},
+		{
+			name:          "RowsAffected() error",
+			expectedError: testErr,
+			prepare: func(u User) {
+				mock.ExpectExec("UPDATE users SET (.*) WHERE id = ?").
+					WithArgs(u.Email, u.Password, u.ID).
+					WillReturnResult(sqlmock.NewErrorResult(testErr))
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.prepare(tc.args)
+
+			err := UpdateUser(db, tc.args)
+			if !errors.Is(err, tc.expectedError) {
+				t.Errorf("expected error to be %v, but got %v", tc.expectedError, err)
 			}
 
 			if err := mock.ExpectationsWereMet(); err != nil {
