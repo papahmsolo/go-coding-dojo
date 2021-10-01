2bb96f30079bb2de5cc38c445f6dedca6720f87b
Bahdan Matviyets <Bahdan.Matviyets@itechart-group.com>
2021-10-01 12:11:32 +0300

    add lru cache problem implementation and tests

---
+++ lru-cache/lru_test.go
@@ -0,0 +1,54 @@
+package lru
+
+import "testing"
+
+func TestLRU(t *testing.T) {
+	cases := []struct {
+		accessSequence []string
+		expectedOrder  string
+	}{
+		{
+			accessSequence: []string{},
+			expectedOrder:  "",
+		},
+		{
+			accessSequence: []string{"A"},
+			expectedOrder:  "A",
+		},
+		{
+			accessSequence: []string{"A", "A", "A"},
+			expectedOrder:  "A",
+		},
+		{
+			accessSequence: []string{"A", "B", "C"},
+			expectedOrder:  "A-B-C",
+		},
+		{
+			accessSequence: []string{"A", "B", "C", "D", "E"},
+			expectedOrder:  "A-B-C-D-E",
+		},
+		{
+			accessSequence: []string{"A", "B", "C", "D", "E", "F"},
+			expectedOrder:  "B-C-D-E-F",
+		},
+		{
+			accessSequence: []string{"A", "B", "A", "C", "A", "B"},
+			expectedOrder:  "C-A-B",
+		},
+		{
+			accessSequence: []string{"A", "B", "A", "B"},
+			expectedOrder:  "A-B",
+		},
+		{
+			accessSequence: []string{"A", "B", "C", "D", "E", "D", "Q", "Z", "C"},
+			expectedOrder:  "E-D-Q-Z-C",
+		},
+	}
+
+	for _, tt := range cases {
+		order := LRUCache(tt.accessSequence)
+		if order != tt.expectedOrder {
+			t.Errorf("expected %q, but got %q", tt.expectedOrder, order)
+		}
+	}
+}
