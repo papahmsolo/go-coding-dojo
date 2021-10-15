1bff420d6b3db1bc22ee1d1375ff15af1e1a0013
xartishuk <xartishuk@gmail.com>
2021-10-15 12:23:25 +0300

    feat(lru-cache): add skip lru implementation

---
+++ lru-cache/skip.go
@@ -0,0 +1,40 @@
+package lru
+
+import "strings"
+
+type SkipLRU struct{}
+
+func (lru SkipLRU) LRUCache(calls []string) string {
+	reversedResult := make([]string, 0, cacheSize)
+
+	// from the end of calls, if not in result - add
+	for i := len(calls) - 1; i >= 0; i-- {
+		call := calls[i]
+		if !contains(reversedResult, call) {
+			reversedResult = append(reversedResult, call)
+		}
+
+		// got full result - breaking
+		if len(reversedResult) == cap(reversedResult) {
+			break
+		}
+	}
+
+	// reversing
+	result := make([]string, 0, len(reversedResult))
+	for i := len(reversedResult) - 1; i >= 0; i-- {
+		result = append(result, reversedResult[i])
+	}
+
+	return strings.Join(result, "-")
+}
+
+func contains(arr []string, elem string) bool {
+	for _, v := range arr {
+		if v == elem {
+			return true
+		}
+	}
+
+	return false
+}
