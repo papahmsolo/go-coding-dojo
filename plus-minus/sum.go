1ba70893179160005ae23b888270370ab0a16030
Ivan Tarbakou <ivantarbakou@gmail.com>
2021-10-22 12:01:20 +0300

    feat(PLUSMINUS): common bitmap solution DRAFT

--- plus-minus/sum.go
+++ plus-minus/sum.go
@@ -1,23 +1,56 @@
 package plus
 
+const (
+	plus = 0
+
+	notPossible = "not possible"
+)
+
 func PlusMinus(n int) string {
 	if n < 0 {
-		return "not possible"
+		return notPossible
 	}
 	if n == 0 {
 		return ""
 	}
 
+	// 123
 	var digits []int
 	for n > 0 {
 		digits = append(digits, n%10)
 		n /= 10
 	}
 
+	// 19945 -> l = 4
 	l := len(digits) - 1
 
+	max := 0
+	solution := ""
 	for i := 0; i < 1<<l; i++ {
 		sum := 0
+		minuses := 0
+		currentSolution := ""
+
+		for j := 0; j < l; j++ {
+			sign := (i >> j) & 1
+			if sign == plus {
+				sum += digits[j]
+				currentSolution += "+"
+			} else {
+				sum -= digits[j]
+				minuses++
+				currentSolution += "-"
+			}
+		}
+
+		if sum == 0 && minuses > max {
+			max = minuses
+			solution = currentSolution
+		}
+	}
+
+	if solution == "" {
+		return notPossible
 	}
 
 	return ""
